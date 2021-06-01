9c63e3baf98a00759c27423ce6b19657a27d0726
Manish Tiwari <76205618+techiemanish@users.noreply.github.com>
2021-06-01 10:36:33 +0530

    Add files via upload
    Custom Event Handling in Java

---
+++ Custom Event Handling in Spring/CustomEventHandler.java
@@ -0,0 +1,8 @@
+import org.springframework.context.ApplicationListener;
+
+public class CustomEventHandler implements ApplicationListener<CustomEvent>{
+    public void onApplicationEvent(CustomEvent event){
+        System.out.println(event.toString());
+    }
+    
+}
